3258729fbfe0ac337b880deb73b38481c77af362
Nandavardhan Reddy Kopparthi <nandavardhan48@gmail.com>
2025-08-14 15:11:17 +0200

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 from dotenv import load_dotenv
 
@@ -11,17 +10,3 @@ class Settings:
     MODEL_PATH = 'app/models/mdel.pkl'
 
 settings = Settings()
-=======
-import os
-from dotenv import load_dotenv
-
-class Settings:
-    PROJECT_NAME = "Capstone_Project"
-    API_KEY = os.getenv("API_KEY", "demokey")
-    JWT_KEY = os.getenv("JWT_KEY", "secret")
-    REDIS_URL =os.getenv("REDIS_URL", "redis://localhost.6379")
-    JWT_ALGORITHM = "HS256"
-    MODEL_PATH = 'app/models/mdel.pkl'
-
-settings = Settings()
->>>>>>> e6cb0c2e6243c8d56831456aaf99109a2cdeeb58
