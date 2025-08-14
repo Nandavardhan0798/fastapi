1fccf5a9703e903a01f4b1d63c76d03e12d3c958
Nandavardhan0798 <nandavardhan48@gmail.com>
2025-08-14 15:16:41 +0200

    Resolved merge conflict in config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,17 +1,9 @@
 <<<<<<< HEAD
-import os
-from dotenv import load_dotenv
-
-class Settings:
-    PROJECT_NAME = "Capstone_Project"
-    API_KEY = os.getenv("API_KEY", "demokey")
-    JWT_KEY = os.getenv("JWT_KEY", "secret")
-    REDIS_URL =os.getenv("REDIS_URL", "redis://localhost.6379")
-    JWT_ALGORITHM = "HS256"
-    MODEL_PATH = 'app/models/mdel.pkl'
-
-settings = Settings()
+# Your local changes
 =======
+# Changes from GitHub
+>>>>>>> origin/main
+
 import os
 from dotenv import load_dotenv
 
@@ -24,4 +16,3 @@ class Settings:
     MODEL_PATH = 'app/models/mdel.pkl'
 
 settings = Settings()
->>>>>>> e6cb0c2e6243c8d56831456aaf99109a2cdeeb58
